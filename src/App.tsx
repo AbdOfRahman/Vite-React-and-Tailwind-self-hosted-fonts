c995101f6ee19ee2cae7b1b2090d4163bd34cbb4
Abdulrahman Ahmed <93364766+AbdOfRahman@users.noreply.github.com>
2023-04-28 11:40:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
           Hello world!
         </h1>
         <h1 className="text-3xl font-bold underline font-sans">
-          fHello world!
+          Hello world!
         </h1>
       </div>
     </>
